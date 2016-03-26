67491fc068da9339af06da4472ce47377eb5ff7c
ocefpaf <ocefpaf@gmail.com>
2016-03-26 16:32:36 -0300

    rerender

--- ci_support/run_docker_build.sh
+++ ci_support/run_docker_build.sh
@@ -5,9 +5,6 @@
 FEEDSTOCK_ROOT=$(cd "$(dirname "$0")/.."; pwd;)
 RECIPE_ROOT=$FEEDSTOCK_ROOT/recipe
 
-UPLOAD_OWNER="conda-forge"
-UPLOAD_CHANNEL="main"
-
 docker info
 
 config=$(cat <<CONDARC
@@ -30,83 +27,42 @@ cat << EOF | docker run -i \
                         -v ${RECIPE_ROOT}:/recipe_root \
                         -v ${FEEDSTOCK_ROOT}:/feedstock_root \
                         -a stdin -a stdout -a stderr \
-                        pelson/conda64_obvious_ci \
-                        bash || exit $?
-
-export PYTHONUNBUFFERED=1
-echo "$config" > ~/.condarc
-# A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
-conda clean --lock
-
-conda info
-
-
-# Embarking on 4 case(s).
-
-    set -x
-    export CONDA_PY=2.7
-    export CONDA_NPY=1.8
-    set +x
-    conda build --no-test /recipe_root || exit 1
-    
-
-    set -x
-    export CONDA_PY=2.7
-    export CONDA_NPY=1.9
-    set +x
-    conda build --no-test /recipe_root || exit 1
-    
-
-    set -x
-    export CONDA_PY=3.4
-    export CONDA_NPY=1.8
-    set +x
-    conda build --no-test /recipe_root || exit 1
-    
-
-    set -x
-    export CONDA_PY=3.4
-    export CONDA_NPY=1.9
-    set +x
-    conda build --no-test /recipe_root || exit 1
-    
-EOF
-
-
-# In a separate docker, run the test...
-cat << EOF | docker run -i \
-                        -v ${RECIPE_ROOT}:/recipe_root \
-                        -v ${FEEDSTOCK_ROOT}:/feedstock_root \
-                        -a stdin -a stdout -a stderr \
-                        pelson/conda64_obvious_ci \
+                        pelson/obvious-ci:latest_x64 \
                         bash || exit $?
 
 export BINSTAR_TOKEN=${BINSTAR_TOKEN}
 export PYTHONUNBUFFERED=1
+
 echo "$config" > ~/.condarc
+# A lock sometimes occurs with incomplete builds. The lock file is stored in build_artefacts.
+conda clean --lock
+
+conda update --yes --all
 
 conda info
 
 
-    export CONDA_PY=2.7
-    export CONDA_NPY=1.8
-    
-    conda build --test /recipe_root || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root $UPLOAD_OWNER --channel=$UPLOAD_CHANNEL || exit 1
-    export CONDA_PY=2.7
-    export CONDA_NPY=1.9
-    
-    conda build --test /recipe_root || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root $UPLOAD_OWNER --channel=$UPLOAD_CHANNEL || exit 1
-    export CONDA_PY=3.4
-    export CONDA_NPY=1.8
-    
-    conda build --test /recipe_root || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root $UPLOAD_OWNER --channel=$UPLOAD_CHANNEL || exit 1
-    export CONDA_PY=3.4
-    export CONDA_NPY=1.9
-    
-    conda build --test /recipe_root || exit 1
-    /feedstock_root/ci_support/upload_or_check_non_existence.py /recipe_root $UPLOAD_OWNER --channel=$UPLOAD_CHANNEL || exit 1
 
+# Embarking on 3 case(s).
+
+    set -x
+    export CONDA_PY=27
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    
+    
+
+    set -x
+    export CONDA_PY=34
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    
+    
+
+    set -x
+    export CONDA_PY=35
+    set +x
+    conda build /recipe_root --quiet || exit 1
+    
+    
 EOF
